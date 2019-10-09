f09b2ea400df5b62ae177fd99ac3a6e598d9876e
Alexander Ahrens <alexander.m.ahrens@gmail.com>
2019-10-09 16:02:22 -0500

    Created albums_migration.sql and created and filled CREATE TABLE named albums

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+CREATE TABLE albums (
+    column1_name data_type,
+    column2_name data_type,
+    id int(10),
+    artist varchar(50),
+    name varchar (25),
+    release_date(4),
+    sales float(1000),
+    genre varchar(20)
+
+    SHOW CREATE TABLE quotes;
+);
